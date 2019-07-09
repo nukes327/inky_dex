99209b4e8296c7c6c843c39437b8ad305d7889c6
nukes327 <jake.nukes327@gmail.com>
2019-07-08 23:27:05 -0400

    Current display test script, and updated touchup list

--- display-test.py
+++ display-test.py
@@ -1,9 +1,27 @@
+import time
 from inky import InkyPHAT
-from PIL import Image
+from PIL import Image, ImageDraw
 
 inky_display = InkyPHAT('yellow')
 inky_display.set_border(inky_display.BLACK)
 
-img = Image.open('mass-test.png')
-inky_display.set_image(img)
-inky_display.show()
+img = Image.open('dex-background.png')
+
+for i in (2, 3, 5, 6, 8, 9):
+    try:
+        sprite = Image.open('sprites/{0:03d}-2s.png'.format(i))
+    except IOError:
+        continue
+    
+    if sprite is not None:
+        print('Current sprite is {0:03d}-2s.png'.format(i))
+        img.paste(sprite, (6, 7))
+        inky_display.set_image(img)
+        inky_display.show()
+        time.sleep(1)
+
+#sprite = Image.open('sprites/004-2s.png')
+
+#img.paste(sprite, (6 , 7))
+#inky_display.set_image(img)
+#inky_display.show()
