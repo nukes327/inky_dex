e4e3b82132cc760063fdc6064cd4f93c205306ed
nukes327 <jake.nukes327@gmail.com>
2019-07-10 01:14:43 -0400

    Added function to split entry into lines
    With function, entry is split into 17 character lines with intelligent wrapping using
    hyphens. With this splitting method an entry should in theory be able to have between
    107 characters (worst case), and 125 characters (best case).
    
    Worst case here is each line ending with a space as the 16th 'character', as spaces at
    the end of a line can be considered free. This leads to two empty spaces at the end of
    each of the first 6 lines for the worst case, as there's no reasonable way to hyphenate
    a word at the end of a line with a space as character 16.
    
    Best case here is each line ending with an actual last character of a word as the 17th
    character on each line. This saves the need to include a space between that word and the
    next, saving 1 space each on the first 6 lines, leading to a max of 125 characters.

--- display-test.py
+++ display-test.py
@@ -5,23 +5,47 @@ from PIL import Image, ImageDraw
 inky_display = InkyPHAT('yellow')
 inky_display.set_border(inky_display.BLACK)
 
-img = Image.open('dex-background.png')
+img = Image.open('dex-mockup.png')
 
-for i in (2, 3, 5, 6, 8, 9):
-    try:
-        sprite = Image.open('sprites/{0:03d}-2s.png'.format(i))
-    except IOError:
-        continue
+def entry_split(entry):
+    words = entry.split(' ')
+    line_index = 0
+    line_strings = ['', '', '', '', '', '', '']
+
+    for i in range(len(words)):
+        if ((len(line_strings[line_index]) == 15 and len(words[i]) > 2)
+         or (len(line_strings[line_index]) == 16 and len(words[i]) > 1)
+         or (len(line_strings[line_index]) == 17)):
+            line_index += 1
+        if len(line_strings[line_index]) + len(words[i]) <= 17:
+            line_strings[line_index] += words[i]
+            if len(line_strings[line_index]) < 17:
+                line_strings[line_index] += ' '
+        else:
+            split_index = 17 - len(line_strings[line_index]) - 1
+            line_strings[line_index] += words[i][:split_index] + '-'
+            line_index += 1
+            line_strings[line_index] += words[i][split_index:] + ' '
     
-    if sprite is not None:
-        print('Current sprite is {0:03d}-2s.png'.format(i))
-        img.paste(sprite, (6, 7))
-        inky_display.set_image(img)
-        inky_display.show()
-        time.sleep(1)
+    return line_strings
+
+#for i in (2, 3, 5, 6, 8, 9):
+#    try:
+#        sprite = Image.open('sprites/{0:03d}-2s.png'.format(i))
+#    except IOError:
+#        continue
+#    
+#    if sprite is not None:
+#        print('Current sprite is {0:03d}-2s.png'.format(i))
+#        img.paste(sprite, (7, 7))
+#        inky_display.set_image(img)
+#        inky_display.show()
+#        time.sleep(1)
 
 #sprite = Image.open('sprites/004-2s.png')
 
-#img.paste(sprite, (6 , 7))
-#inky_display.set_image(img)
-#inky_display.show()
+#img.paste(sprite, (7 , 7))
+inky_display.set_image(img)
+inky_display.show()
+
+print(entry_split('A legend says that its body glows in seven colors. A rainbow is said to form behind it when it flies.'))
