677bfec3a8e511bc5d0406f66f41f039889c4079
nukes327 <jake.nukes327@gmail.com>
2020-06-13 11:46:35 -0400

    Now using font class, image output saved
    Finally got around to actually putting the font class to use instead of 
    the old method calls, so was able to adjust some functions to account 
    for that. There may still be more adjustments and cleanups that can be 
    made but this is a start.
    
    Consolidated the location values in each function call to an XY tuple. 
    This doesn't strictly simplify the initial call necessarily but can 
    simplify later calls within functions.
    
    Black has been adopted for formatting of files so some of that has been 
    changed as well.
    
    Next to do is either implement logging or to clean up entry and numeric 
    display functions to have a few less magic numbers sprinkled around.

--- dex-entry.py
+++ dex-entry.py
@@ -15,8 +15,10 @@ Todo:
 
 from typing import List, Tuple, Dict, Union
 from PIL import Image, ImageDraw  # type: ignore
+from PIL.ImagePalette import ImagePalette
 import re
 import png  # type: ignore
+from dex.font import Font
 
 
 def get_entry(id: int, gen: int, ver: int) -> str:
@@ -41,8 +43,10 @@ def get_entry(id: int, gen: int, ver: int) -> str:
         Actually implement this once a database is in place.
 
     """
-    entry = ("For no reason, it jumps and splashes about, making it "
-             "easy for predators like Pidgeotto to catch it mid-jump.")
+    entry = (
+        "For no reason, it jumps and splashes about, making it "
+        "easy for predators like Pidgeotto to catch it mid-jump."
+    )
     return entry
 
 
@@ -102,8 +106,8 @@ def gsc_format(entry: str) -> str:
         Clean up replacements
 
     """
-    partial = entry.replace("'d", 'ⓓ').replace("'l", 'ⓛ').replace("'m", 'ⓜ').replace("'r", 'ⓡ')
-    finished = partial.replace("'s", 'ⓢ').replace("'t", 'ⓣ').replace("'v", 'ⓥ')
+    partial = entry.replace("'d", "ⓓ").replace("'l", "ⓛ").replace("'m", "ⓜ").replace("'r", "ⓡ")
+    finished = partial.replace("'s", "ⓢ").replace("'t", "ⓣ").replace("'v", "ⓥ")
     return finished
 
 
@@ -131,24 +135,26 @@ def entry_wrap(entry: str, line_length: int = 17, line_count: int = 7) -> List[s
         This can probably be written cleaner, look in to it
 
     """
-    words = entry.split(' ')
+    words = entry.split(" ")
     line_index = 0
-    line_strings = [''] * line_count
+    line_strings = [""] * line_count
 
     for i in range(len(words)):
-        if ((len(line_strings[line_index]) == line_length - 2 and len(words[i]) > 2) or
-           (len(line_strings[line_index]) == line_length - 1 and len(words[i]) > 1) or
-           (len(line_strings[line_index]) == line_length)):
+        if (
+            (len(line_strings[line_index]) == line_length - 2 and len(words[i]) > 2)
+            or (len(line_strings[line_index]) == line_length - 1 and len(words[i]) > 1)
+            or (len(line_strings[line_index]) == line_length)
+        ):
             line_index += 1
         if len(line_strings[line_index]) + len(words[i]) <= line_length:
             line_strings[line_index] += words[i]
             if len(line_strings[line_index]) < line_length:
-                line_strings[line_index] += ' '
+                line_strings[line_index] += " "
         else:
             wrap_index = line_length - len(line_strings[line_index])
-            line_strings[line_index] += words[i][:wrap_index] + '-'
+            line_strings[line_index] += words[i][:wrap_index] + "-"
             line_index += 1
-            line_strings[line_index] += words[i][wrap_index:] + ' '
+            line_strings[line_index] += words[i][wrap_index:] + " "
     return line_strings
 
 
@@ -170,7 +176,7 @@ def create_mask(source: Image, mask: Tuple[int, int, int] = (0, 1, 2)) -> Image:
         their examples for their inky displays.
 
     """
-    mask_image = Image.new('1', source.size)
+    mask_image = Image.new("1", source.size)
     w, h = source.size
     for x in range(w):
         for y in range(h):
@@ -180,59 +186,41 @@ def create_mask(source: Image, mask: Tuple[int, int, int] = (0, 1, 2)) -> Image:
     return mask_image
 
 
-def display_char(img: Image, font: Image, x: int, y: int,
-                 character: str, char_width: int = 8, char_height: int = 8) -> None:
+def display_char(img: Image, font: Font, location: Tuple[int, int], character: str) -> None:
     """Paste a single character from a fontsheet to the display image.
 
     Args:
         img:         The display image to be pasted into
-        font:        The fontsheet image
-        x:           X coordinate to paste character
-        y:           Y coordinate to paste character
+        font:        The font class in use
+        location:    (x, y) location tuple to paste character
         character:   Character to be pasted
-        char_width:  Width of each character on the fontsheet
-                     Default of 8 is the gscfont char width
-        char_height: Height of each character on the fontsheet
-                     Default of 8 is the gscfont char height
 
     Notes:
         X and Y coordinates are anchored to the top left pixel of the character,
         and 0,0 is the top left of the display
 
-    Todo:
-        The sheet variable is used to determine fontsheet location for each
-        character, the issue being that it's hardcoded for the gscfont fontsheet.
-        This should be fixed to be usable for any dimension of fontsheet, either
-        by adding another method argument, or planning some more clever way.
-
-        The magic number 16 when determining the sheet x and y is also hardcoded
-        based on the character width of the gscfont file. This should also be
-        fixed to be more versatile.
-
     """
-    sheet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ():;[]abcdefghijklmnopqrstuvwxyz      "
-             "ÄÖÜäöü          ⓓⓛⓜⓡⓢⓣⓥ       ⓃⓄ'①②-  ?!.&é ▷▶▼♂$×./,♀0123456789")
-    sheet_x = (sheet.index(character) % 16) * char_width
-    sheet_y = (int(sheet.index(character) / 16)) * char_height
+    # sheet = font.sheetstring
+    # (
+    #     "ABCDEFGHIJKLMNOPQRSTUVWXYZ():;[]abcdefghijklmnopqrstuvwxyz      "
+    #     "ÄÖÜäöü          ⓓⓛⓜⓡⓢⓣⓥ       ⓃⓄ'①②-  ?!.&é ▷▶▼♂$×./,♀0123456789"
+    # )
+    # sheet_x = (sheet.index(character) % font.sheetwidth) * font.charwidth
+    # sheet_y = (int(sheet.index(character) / font.sheetwidth)) * font.charheight
 
-    char_sprite = font.crop((sheet_x, sheet_y, sheet_x + char_width, sheet_y + char_height))
-    img.paste(char_sprite, (x, y), create_mask(char_sprite))
+    # char_sprite = font.crop((sheet_x, sheet_y, sheet_x + char_width, sheet_y + char_height))
+    char_sprite = font.get_character(character)
+    img.paste(char_sprite, location, create_mask(char_sprite))
 
 
-def display_line(img: Image, font: Image, x: int, y: int,
-                 line: str, char_width: int = 8, char_height: int = 8) -> None:
+def display_line(img: Image, font: Font, location: Tuple[int, int], line: str) -> None:
     """Paste an entire line into a display image using characters from a fontsheet.
 
     Args:
         img:         The display image to be pasted into
-        font:        The fontsheet image
-        x:           X coordinate to paste line
-        y:           Y coordinate to paste line
+        font:        The font class
+        location:    (x, y) location tuple to paste character
         line:        Line to be pasted
-        char_width:  Width of each character on the fontsheet
-                     Default of 8 is the gscfont char width
-        char_height: Height of each character on the fontsheet
-                     Default of 8 is the gscfont char height
 
     Notes:
         X and Y coordinates are anchored to the top left pixel of the
@@ -240,23 +228,17 @@ def display_line(img: Image, font: Image, x: int, y: int,
 
     """
     for character, index in zip(line, range(len(line))):
-        display_char(img, font, x + char_width * index, y, character, char_width, char_height)
+        display_char(img, font, (location[0] + font.charwidth * index, location[1]), character)
 
 
-def display_lines(img: Image, font: Image, x: int, y: int, lines: List[str],
-                  char_width: int = 8, char_height: int = 8, line_gap: int = 4) -> None:
+def display_lines(img: Image, font: Font, location: Tuple[int, int], lines: List[str], line_gap: int = 4,) -> None:
     """Paste multiple lines into a display image using characters from a fontsheet.
 
     Args:
         img:         The display image to be pasted into
-        font:        The fontsheet image
-        x:           X coordinate to paste lines
-        y:           Y coordinate to paste lines
+        font:        The font class
+        location:    (x, y) location tuple to paste character
         lines:       List of lines to be pasted
-        char_width:  Width of each character on the fontsheet
-                     Default of 8 is the gscfont char width
-        char_height: Height of each character on the fontsheet
-                     Default of 8 is the gscfont char height
         line_gap:    Distance in pixels to separate each line
 
     Notes:
@@ -267,85 +249,10 @@ def display_lines(img: Image, font: Image, x: int, y: int, lines: List[str],
 
     """
     for line, index in zip(lines, range(len(lines))):
-        display_line(img, font, x, y + index * (char_height + line_gap), line, char_width, char_height)
+        display_line(img, font, (location[0], location[1] + index * (font.charheight + line_gap)), line)
 
 
-def get_font_metadata(font: str) -> Dict[str, Union[int, str]]:
-    """Extract metadata from font image.
-
-    Args:
-        font: The filename for the sheet to be used
-
-    Returns:
-        Dictionary containing the following pairs:
-            CHARWIDTH:   int
-            CHARHEIGHT:  int
-            SHEETWIDTH:  int
-            SHEETSTRING: str
-
-    Notes:
-        The returned values are decoded from ancillary chunks in the font PNG
-        The location or order of the chunks doesn't matter as long as it passes PNG spec,
-        The chunks be formatted as follows:
-
-            Chunk Flag:
-                tEXt
-            Keyword:
-                CHARWIDTH
-            Text:
-                integer width in pixels of individual character
-
-            Chunk Flag:
-                tEXt
-            Keyword:
-                CHARHEIGHT
-            Text:
-                integer height in pixels of individual character
-
-            Chunk Flag:
-                tEXt
-            Keyword:
-                SHEETWIDTH
-            Text:
-                integer width in characters of the fontsheet
-
-            Chunk Flag:
-                iTXt
-            Keyword:
-                SHEETSTRING
-            Text:
-                string containing all characters, left to right, top down
-
-    Todo:
-        Implement metadata extraction
-        Add necessary chunks to extant fontsheets
-        Add more documentation on fontsheet requirements:
-            Move some documentation out of this docstring?
-            Document required unicode characters for special sheet characters
-
-    """
-    sheet = png.Reader(filename=font)
-    chunk_list = list(sheet.chunks())
-    metadata = {}
-
-    # compare chunks to regexes, add as key: value pairs to a dictionary
-    # return in proper order
-    for chunk in chunk_list:
-        if re.compile(b'..Xt').match(chunk[0]):
-            decoded = bytes.decode(chunk[1]).split('\x00')
-            keyword = decoded[0]
-            value = decoded[-1]
-            if keyword != 'SHEETSTRING':
-                metadata[keyword] = int(value)
-            else:
-                metadata[keyword] = value
-
-    return metadata
-    # return (8, 8, 16, "ABCDEFGHIJKLMNOPQRSTUVWXYZ():;[]abcdefghijklmnopqrstuvwxyz      "
-    #         "ÄÖÜäöü          ⓓⓛⓜⓡⓢⓣⓥ       ⓃⓄ'①②-  ?!.&é ▷▶▼♂$×./,♀0123456789")
-
-
-def display_sprite(img: Image, x: int, y: int, id: int, gen: int, ver: int, form: int = 0) -> None:
+def display_sprite(img: Image, location: Tuple[int, int], id: int, gen: int, ver: int, form: int = 0) -> None:
     """Paste sprite and bounding box into display image.
 
     Args:
@@ -369,49 +276,49 @@ def display_sprite(img: Image, x: int, y: int, id: int, gen: int, ver: int, form
         as they are gen 2, but it would help expansion in the future if done now.
 
     """
-    box = Image.open('assets/ui/spritebox.png')
-    sprite_sheet = Image.open('assets/sprites/{:03d}.png'.format(id))
+    box = Image.open("assets/ui/spritebox.png")
+    sprite_sheet = Image.open("assets/sprites/{:03d}.png".format(id))
 
     # TODO: Put this next line in a try/except or get index errors for high gens
     sprite = sprite_sheet.crop((0, 56 * ver, 56, 56 + 56 * ver))
 
-    img.paste(box, (x, y), create_mask(box))
-    img.paste(sprite, (x + 6, y + 6), create_mask(sprite))
+    img.paste(box, location, create_mask(box))
+    img.paste(sprite, tuple(n + 6 for n in location), create_mask(sprite))
 
 
-def display_footprint(img: Image, x: int, y: int, id: int) -> None:
+def display_footprint(img: Image, location: Tuple[int, int], id: int) -> None:
     """Paste footprint sprite into display image.
 
     Args:
-        img: The display image to be pasted into
-        x:   X coordinate to paste footprint
-        y:   Y coordinate to paste footprint
-        id:  Id of the pokemon footprint to paste
+        img:      The display image to be pasted into
+        location: (x, y) location tuple to paste character
+        id:       Id of the pokemon footprint to paste
 
     Notes:
         X and Y coordinates are anchored to the top left of the footprint
         sprite area, 0,0 is the top left of the display.
 
     """
-    sprite_sheet = Image.open('assets/sprites/{:03d}.png'.format(id))
+    sprite_sheet = Image.open("assets/sprites/{:03d}.png".format(id))
     footprint = sprite_sheet.crop((0, 112, 16, 128))
-    img.paste(footprint, (x, y), create_mask(footprint))
+    img.paste(footprint, location, create_mask(footprint))
 
 
-def display_numeric(img: Image, font: Image, x: int, y: int, width: int, data: Tuple[int, float, float]) -> None:
+def display_numeric(
+    img: Image, font: Font, location: Tuple[int, int], width: int, data: Tuple[int, float, float]
+) -> None:
     """Paste numeric data into display image.
 
     Args:
-        img:   Display image to be pasted into
-        font:  Fontsheet image
-        x:     X coordinate to paste data
-        y:     Y coordinate to paste data
-        width: How wide the section should be
-        data:  Numeric data to be pasted
-               Tuple with data in the following order:
-                 ID
-                 Height
-                 Weight
+        img:      Display image to be pasted into
+        font:     Fontsheet image
+        location: (x, y) location tuple to paste character
+        width:    How wide the section should be
+        data:     Numeric data to be pasted
+                  Tuple with data in the following order:
+                    ID
+                    Height
+                    Weight
 
     Notes:
         X and Y coordinates are the top left of the ID line for numeric data
@@ -425,50 +332,65 @@ def display_numeric(img: Image, font: Image, x: int, y: int, width: int, data: T
     """
     draw = ImageDraw.Draw(img)
     underline = 2  # Third color on a three color inky display
+    x = location[0]
+    y = location[1]
     ly = y
     right_bound = x + width
 
     # ID section
-    display_line(img, font, x, ly, 'ⓃⓄ')
-    display_line(img, font, right_bound - (3 * 8), ly, '{0:03d}'.format(data[0]))
+    display_line(img, font, (x, ly), "ⓃⓄ")
+    display_line(img, font, (right_bound - (3 * 8), ly), "{0:03d}".format(data[0]))
     draw.line((x, ly + 8, right_bound - 2, ly + 8), underline)
 
     # Height section
     ly += 8 + 4
-    display_line(img, font, x, ly, 'HT')
-    display_char(img, font, right_bound - 8, ly, 'm')
-    display_char(img, font, int(right_bound - 2.75 * 8), ly, '.')
-    display_line(img, font, int(right_bound - 4.5 * 8), ly, '{: >2s}'.format(str(data[1]).split('.')[0]))
-    display_line(img, font, int(right_bound - 2.125 * 8), ly, str(data[1]).split('.')[1])
+    display_line(img, font, (x, ly), "HT")
+    display_char(img, font, (right_bound - 8, ly), "m")
+    display_char(img, font, (int(right_bound - 2.75 * 8), ly), ".")
+    display_line(img, font, (int(right_bound - 4.5 * 8), ly), "{: >2s}".format(str(data[1]).split(".")[0]))
+    display_line(img, font, (int(right_bound - 2.125 * 8), ly), str(data[1]).split(".")[1])
     draw.line((x, ly + 8, right_bound - 2, ly + 8), underline)
 
     # Weight section
     ly += 8 + 4
-    display_line(img, font, x, ly, 'WT')
-    display_char(img, font, right_bound - 2 * 8 + 1, ly, 'k')
-    display_char(img, font, right_bound - 8, ly, 'g')
-    display_char(img, font, int(right_bound - 3.5 * 8), ly, '.')
-    display_line(img, font, int(right_bound - 6.25 * 8), ly, '{: >3s}'.format(str(data[2]).split('.')[0]))
-    display_line(img, font, int(right_bound - 2.875 * 8), ly, str(data[2]).split('.')[1])
+    display_line(img, font, (x, ly), "WT")
+    display_char(img, font, (right_bound - 2 * 8 + 1, ly), "k")
+    display_char(img, font, (right_bound - 8, ly), "g")
+    display_char(img, font, (int(right_bound - 3.5 * 8), ly), ".")
+    display_line(img, font, (int(right_bound - 6.25 * 8), ly), "{: >3s}".format(str(data[2]).split(".")[0]))
+    display_line(img, font, (int(right_bound - 2.875 * 8), ly), str(data[2]).split(".")[1])
     draw.line((x, ly + 8, right_bound - 2, ly + 8), underline)
 
 
-if __name__ == '__main__':
-    from inky import InkyPHAT  # type: ignore
-    inky_display = InkyPHAT('yellow')
-    inky_display.set_border(inky_display.BLACK)
-    img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
-    font = Image.open('assets/ui/gscfont.png')
+if __name__ == "__main__":
+    try:
+        from inky import InkyPHAT  # type: ignore
+    except ModuleNotFoundError:
+        print("Inky library absent, will not display to e-ink")
+        print("Setting image size to PHAT")
+        img = Image.new("P", (212, 104))
+        en_inky = False
+    else:
+        en_inky = True
+        inky_display = InkyPHAT("yellow")
+        inky_display.set_border(inky_display.BLACK)
+        img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 
     id = 129
 
     entry = get_entry(id, 2, 0)
     data = get_data(id)
-    font_data = get_font_metadata('assets/ui/gscfont.png')
+    font = Font("assets/ui/gscfont.png")
+    print(font)
 
-    display_sprite(img, 1, 1, id, 2, 0)
-    display_numeric(img, font, 2, 69, 67, (id, data[2], data[3]))
+    display_sprite(img, (1, 1), id, 2, 0)
+    display_numeric(img, font, (2, 69), 67, (id, data[2], data[3]))
 
     img = img.rotate(180)
-    inky_display.set_image(img)
-    inky_display.show()
+    if en_inky:
+        inky_display.set_image(img)
+        inky_display.show()
+    palette = ImagePalette(palette=[255, 255, 255, 0, 0, 0, 255, 0, 0], size=9)
+    img.putpalette(palette.tostring())
+    img = img.rotate(180)
+    img.save("last_display.png")
