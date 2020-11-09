dc0b38da40c6247961ff590f1697cf2a5eca97dc
nukes327 <jake.nukes327@gmail.com>
2020-11-09 14:16:24 -0500

    Slight expansion of pokemon class
    Expanded parameters to include all data passed around short
    of sprites, which may be added at a future date.
    
    Documented expansion of attributes.

--- dex/poke.py
+++ dex/poke.py
@@ -12,20 +12,36 @@ Notes:
 
 """
 
+from typing import Dict, Tuple
+
+
 class Pokemon:
-    """Pokemon object for data consolidation."""
+    """Pokemon object for data consolidation.
+
+    Attributes:
+        number (int): National dex id for pokemon
+        species (str): Species (often seen as name) of pokemon
+        classification (str): How the dex classifies species, i.e. mouse pokemon pikachu
+        weight (float): Weight in kg
+        height (float): Height in meters
+        entries (dict of tuple/str pairs): All dex entries compiled for species
+            Tuple is (gen, ver)
+
+    """
 
     def __init__(self, number: int):
         """Initialize values for pokemon object.
 
         Args:
-            number: Dex number of pokemon to create
+            number: National dex number of pokemon to create
 
         """
         self.number = number
+        self.species: str
+        self.classification: str
         self.weight: float
         self.height: float
-        self.entry: str
+        self.entries: Dict[Tuple[int, int], str]
         self.load_data()
 
     def load_data(self) -> None:
